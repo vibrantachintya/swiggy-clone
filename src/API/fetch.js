2968c38559fc5fcaf4427b947fbc52d0bb5ba340
vibrantachintya <vibrantachintya@gmail.com>
2022-04-06 12:02:43 +0530

    fetch url fixed

--- src/API/fetch.js
+++ src/API/fetch.js
@@ -1,43 +1,45 @@
 import axios from "axios";
 
+const url = "https://json-server-achintya.herokuapp.com/";
+
 export const fetchRestaurantsByCity = async (city) => {
 
-    let res = await axios.get(`http://localhost:8080/restaurants`);
+    let res = await axios.get(`${url}restaurants`);
     return res.data;
 
 };
 
 export const fetchRestaurantById = async (id) => {
 
-    let res = await axios.get(`http://localhost:8080/restaurants/${id}`);
+    let res = await axios.get(`${url}restaurants/${id}`);
     return res.data;
     
 };
 
 export const fetchCuisines = async (id) => {
 
-    let res = await axios.get(`http://localhost:8080/cuisines`);
+    let res = await axios.get(`${url}cuisines`);
     return res.data;
     
 };
 
 export const fetchDishes = async (id) => {
 
-    let res = await axios.get(`http://localhost:8080/dishes`);
+    let res = await axios.get(`${url}dishes`);
     return res.data;
     
 };
 
 export const createUser = async (user) => {
 
-    let res = await axios.post(`http://localhost:8080/swiggy_user`, user);
+    let res = await axios.post(`${url}swiggy_user`, user);
     return res.data;
     
 };
 
 export const loginUser = async (email, pwd) => {
     try {
-        let res = await axios.get(`http://localhost:8080/swiggy_user?email=${email}`);
+        let res = await axios.get(`${url}swiggy_user?email=${email}`);
         if(res.data.length > 0 && res.data[0].pwd == pwd) return res.data[0];
         return "invalid";
     } catch(e) {
